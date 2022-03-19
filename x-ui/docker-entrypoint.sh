367f0d891621d7c60d2a0cd3a6adca4619ca761c
kissyouhunter <kissyouhunter@hotmail.com>
2022-03-19 19:04:52 +0800

    upload

---
+++ x-ui/docker-entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+TIME() {
+[[ -z "$1" ]] && {
+	echo -ne " "
+} || {
+     case $1 in
+	r) export Color="\e[31;1m";;
+	g) export Color="\e[32;1m";;
+	b) export Color="\e[34;1m";;
+	y) export Color="\e[33;1m";;
+	z) export Color="\e[35;1m";;
+	l) export Color="\e[36;1m";;
+	w) export Color="\e[29;1m";;
+      esac
+	[[ $# -lt 2 ]] && echo -e "\e[36m\e[0m ${1}" || {
+		echo -e "\e[36m\e[0m ${Color}${2}\e[0m"
+	 }
+      }
+}
+
+x-ui restart
+x-ui enable
+
+TIME g "默认账号：admin"
+TIME g "默认密码：admin"
+TIME g "默认端口：54321"
+
+tail -f /dev/null;
+
