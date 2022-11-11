ecffc299095543b16e581b4aadf0e6fc7e1f5d06
kissyouhunter <kissyouhunter@hotmail.com>
2022-11-11 18:40:53 +0800

    update

--- qBittorrent/run.sh
+++ qBittorrent/run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+if [ -f "/config/rclone-upload.sh" ]; then
+	echo
+else
+	wget -O /config/rclone-upload.sh https://github.com/kissyouhunter/Tools/raw/main/rclone/rclone-upload.sh
+	chmod +x /config/rclone-upload.sh
+fi
+
 cd /app && python main.py 
